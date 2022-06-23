b32898fc62bca451d1f40a2f0f14b1c3961ff7d4
JaydenDev <willsjayden2@gmail.com>
2022-06-23 13:40:00 -0500

    chore: add warnings to installer

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo rm -rf /home/$USER/.local/bin/pclish_dir
 git clone https://github.com/JaydenDev/pclish /tmp/pclish
 mkdir /home/$USER/.local/bin/pclish_dir
 pyinstaller /tmp/pclish/pclish.py --distpath=/tmp/pclish_dir
+echo "If you said yes to deleting the output directory, you must re-run the installer and say no."
 mv /tmp/pclish_dir/pclish /home/$USER/.local/bin/pclish_dir
 ln -sf /home/$USER/.local/bin/pclish_dir/pclish/pclish /home/$USER/.local/bin/pclish
 chmod +x /home/$USER/.local/bin/pclish
