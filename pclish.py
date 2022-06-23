4c69756273f5f127f66ff4637bf1a20a8abda66f
JaydenDev <willsjayden2@gmail.com>
2022-06-23 13:52:24 -0500

    yayy

--- pclish.py
+++ pclish.py
@@ -83,7 +83,7 @@ def main():
         elif inp == "system":
             pclish_system()
         else:
-            os.system("SHELL='pclish' && " + inp)
+            os.system("SHELL='/bin/pclish' && " + inp)
 
 #  Main
 if '__main__' == __name__:
