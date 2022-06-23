c8f13548e8d8280da2cdd19d694d2975f93d121f
JaydenDev <willsjayden2@gmail.com>
2022-06-23 13:09:01 -0500

    feat: improve installation process

--- install.sh
+++ install.sh
@@ -1,9 +1,13 @@
 # PCLISH - Python Command Line Shell
 # Installation Script
 
+pip install pyinstaller
 rm -rf /tmp/pclish
 git clone https://github.com/JaydenDev/pclish /tmp/pclish
-sudo mv /tmp/pclish/shell.py /usr/bin/pclish
+python -m pyinstaller /tmp/pclish/pclish.py
+sudo mkdir /usr/bin/pclish_dir
+sudo mv /tmp/pclish/dist/pclish/* /usr/bin/pclish_dir
+sudo ln -sf /usr/bin/pclish_dir/pclish /usr/bin/pclish
 sudo chmod +x /usr/bin/pclish
 
 
