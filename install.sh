5126ff733f423b257ca41d9f74b72f252ad67de3
JaydenDev <willsjayden2@gmail.com>
2022-06-23 13:18:22 -0500

    fix: fix: fix:

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -rf /tmp/pclish
 sudo rm -rf /usr/bin/pclish
 sudo rm -rf /usr/bin/pclish_dir
 git clone https://github.com/JaydenDev/pclish /tmp/pclish
-python -m pyinstaller /tmp/pclish/pclish.py
+pyinstaller /tmp/pclish/pclish.py
 sudo mkdir /usr/bin/pclish_dir
 sudo mv /tmp/pclish/dist/pclish/* /usr/bin/pclish_dir
 sudo ln -sf /usr/bin/pclish_dir/pclish /usr/bin/pclish
