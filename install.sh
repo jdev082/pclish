cd5449e4e79d34ba254281032ac89c19065f6ffd
JaydenDev <willsjayden2@gmail.com>
2022-06-23 12:54:07 -0500

    feat: allow OTA installation

---
+++ install.sh
@@ -0,0 +1,10 @@
+# PCLISH - Python Command Line Shell
+# Installation Script
+
+rm -rf /tmp/pclish
+git clone https://github.com/JaydenDev/pclish /tmp/pclish
+sudo mv /tmp/pclish/shell.py /usr/bin/pclish
+sudo chmod +x /usr/bin/pclish
+
+
+
