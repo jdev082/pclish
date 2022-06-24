04741ac5d06925ae22501602aba048d63ccc36b2
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-06-24 10:28:02 -0500

    chore: update OTA installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 # PCLISH - Python Command Line Shell
 # Installation Script
 
+rm -rf /home/$USER/.local/bin/pclish_dir/
+sudo pip install pyinstaller
 rm -rf /tmp/pclish
+sudo rm -rf /home/$USER/.local/bin/pclish
+sudo rm -rf /home/$USER/.local/bin/pclish_dir
 git clone https://github.com/JaydenDev/pclish /tmp/pclish
-sudo mv /tmp/pclish/shell.py /usr/bin/pclish
-sudo chmod +x /usr/bin/pclish
+mkdir /home/$USER/.local/bin/pclish_dir
+pyinstaller /tmp/pclish/pclish.py --distpath=/tmp/pclish_dir
+echo "If you said yes to deleting the output directory, you must re-run the installer and say no."
+mv /tmp/pclish_dir/pclish /home/$USER/.local/bin/pclish_dir
+ln -sf /home/$USER/.local/bin/pclish_dir/pclish/pclish /home/$USER/.local/bin/pclish
+chmod +x /home/$USER/.local/bin/pclish
 
 
 
