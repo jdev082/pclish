6d5e6c4f532b3d21ab9d2b18faa83813d100d4f4
jdev082 <willsjayden2@gmail.com>
2023-05-18 17:08:40 -0500

    fix: follow xdg standards

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 # PCLISH - Python Command Line Shell
 # Installation Script
 
-rm -rf /home/$USER/.local/bin/pclish_dir/
+rm -rf $HOME/.local/bin/pclish_dir/
 sudo pip install pyinstaller
 rm -rf /tmp/pclish
-sudo rm -rf /home/$USER/.local/bin/pclish
-sudo rm -rf /home/$USER/.local/bin/pclish_dir
+sudo rm -rf $HOME/.local/bin/pclish
+sudo rm -rf $HOME/.local/bin/pclish_dir
 git clone https://github.com/JaydenDev/pclish /tmp/pclish
-mkdir /home/$USER/.local/bin/pclish_dir
+mkdir $HOME/.local/bin/pclish_dir
 pyinstaller /tmp/pclish/pclish.py --distpath=/tmp/pclish_dir
 echo "If you said yes to deleting the output directory, you must re-run the installer and say no."
-mv /tmp/pclish_dir/pclish /home/$USER/.local/bin/pclish_dir
-ln -sf /home/$USER/.local/bin/pclish_dir/pclish/pclish /home/$USER/.local/bin/pclish
-chmod +x /home/$USER/.local/bin/pclish
+mv /tmp/pclish_dir/pclish $HOME/.local/bin/pclish_dir
+ln -sf $HOME/.local/bin/pclish_dir/pclish/pclish $HOME/.local/bin/pclish
+chmod +x $HOME/.local/bin/pclish
